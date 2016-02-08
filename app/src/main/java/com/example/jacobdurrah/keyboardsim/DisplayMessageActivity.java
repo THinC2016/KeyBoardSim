182855808ff6afaf284c5654153ac3fb69cf1fd8
Jacob Durrah <jdurrah@umich.edu>
2016-02-08 02:18:15 -0500

    first page done. Working to add google spreadsheet entry into the code such that all collected data will go straight to the google drive :)

--- app/src/main/java/com/example/jacobdurrah/keyboardsim/DisplayMessageActivity.java
+++ app/src/main/java/com/example/jacobdurrah/keyboardsim/DisplayMessageActivity.java
@@ -6,9 +6,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.util.Date;
+
+
+
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -19,15 +29,8 @@ public class DisplayMessageActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
+/*
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
@@ -36,6 +39,43 @@ public class DisplayMessageActivity extends AppCompatActivity {
         textView.setText(message);
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
+        */
+
+
+        //get username from intent
+        Intent intent = getIntent();
+        String userName = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        //createFileName
+        final String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+        final String fileName = userName + '_' + currentDateTimeString.toString() + ".txt";
+        File file = new File(this.getFilesDir(), fileName);
+        FileUtil.appendStringToFile("hello World", file );
+
+
+        EditText editText = (EditText) findViewById(R.id.sim_editText);
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView changed = (TextView) findViewById(R.id.textView);
+                //changed.setText(Long.toString(System.currentTimeMillis()));
+                changed.setText(currentDateTimeString);
+            }
+        });
+
+
+
     }
 
 
